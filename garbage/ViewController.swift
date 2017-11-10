5624e0bc5bd0b61e4cebe22fe4700d1cf7db4a22
Maoko Furuya <maomaofu@docomo.ne.jp>
2017-11-10 19:35:19 +0900

    Initial Commit

---
+++ garbage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  garbage
+//
+//  Created by Maoko Furuya on 2017/11/10.
+//  Copyright © 2017年 Maoko Furuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
